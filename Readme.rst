7a25ad92cc14438657cd12ae3170c8e206e8cf6f
David Francos Cuartero <me@davidfrancos.net>
2020-12-12 02:33:28 +0100

    Rewritten.
    It's not async-enabled anymore as it didn't have any real reason for it.
    This also seems to fix a bug that exausted the resources in the long
    run.

--- Readme.rst
+++ Readme.rst
@@ -1,22 +1,55 @@
-Asyncio-enabled SwitchBot Meter simple API
-------------------------------------------
+.. image:: ./docs/switchbot.png
 
-This exposes a simple library (loosely based on
-https://github.com/OpenWonderLabs/python-host/ because of the names, uuids and
-temp logic) that will scan for all of your switchbot and allow you to iterate
-trought them.
+**Python Swithbot Meter API**
 
-Usage is pretty straightforward::
+Comprehensible `SwitchBot Meter <https://www.switch-bot.com/products/switchbot-meter) API>`_.
+Read your SwitchBot Meter status in real time via BLE. 
 
-        async for device in DevScanner().scan():
-                print(device.mac)
-                print(device.data['humidity'])
-                print(device.data['temperature']
+|pypi| |release| |downloads| |python_versions| |pypi_versions| |coverage| |actions|
+
+.. |pypi| image:: https://img.shields.io/pypi/l/switchbotmeter
+.. |release| image:: https://img.shields.io/librariesio/release/pypi/switchbotmeter
+.. |downloads| image:: https://img.shields.io/pypi/dm/switchbotmeter
+.. |python_versions| image:: https://img.shields.io/pypi/pyversions/switchbotmeter
+.. |pypi_versions| image:: https://img.shields.io/pypi/v/switchbotmeter
+.. |coverage| image:: https://codecov.io/gh/XayOn/switchbotmeter/branch/develop/graph/badge.svg
+    :target: https://codecov.io/gh/XayOn/switchbotmeter
+.. |actions| image:: https://github.com/XayOn/switchbotmeter/workflows/CI%20commit/badge.svg
+    :target: https://github.com/XayOn/switchbotmeter/actions
+
+Installation
+------------
+
+This library is available on `Pypi <https://pypi.org/project/switchbotmeter/>`_, you can install it directly with pip::
+
+        pip install switchbotmeter
+
+This library acts as a BLE client, so you need a
+BLE-capable device (a bluetooth dongle or integrated)
+
+Usage
+-----
+
+This library exports a DeviceScanner object that will
+dected any SwitchBot Meter devices nearby. 
+Note that you need to have permissions to access your
+bluetooth device, the scope of wich will not be covered by
+this readme :
+
+.. code:: python
+
+    from switchbotmeter import DevScanner
+
+    for current_devices in DevScanner(): 
+        for device in current_devices:
+            print(device)
+            print(f'{device.mac} -> {device.temp}')
 
 
-This library is a WIP and it's completely not production ready.
-I will update it soon, the current TODO list is:
+.. code:: bash
 
-- Add tests
-- Improve the ReadMe
-- Add inline documentation and sphinx
+    <T temp: 19.8 humidity: 73> (c6:97:89:d6:c8:09)
+    c6:97:89:d6:c8:09 -> 19.8
+    ...
+    <T temp: 20.4 humidity: 71> (c6:97:89:d6:c8:09)
+    c6:97:89:d6:c8:09 -> 20.4
---
+++ switchbotmeter/__init__.py
@@ -0,0 +1,113 @@
+from bluepy.btle import Scanner, DefaultDelegate
+import binascii
+import datetime
+
+SERVICE_UUID = 'cba20d00-224d-11e6-9fb8-0002a5d5c51b'
+
+
+class DevScanner(DefaultDelegate):
+    """Device Scanner.
+        
+    Iterate trough this device 
+
+    Arguments:
+
+        device: HCI device to scan on
+        wait: On each scan, how much time to wait for devices
+        *args, **kwargs: DefaultDelegate arguments
+    """
+    def __init__(self, device='hci0', wait=5, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wait_time = int(wait)
+        self.scanner = Scanner().withDelegate(self)
+
+    def __iter__(self):
+        """Use as iterator."""
+        return self
+
+    def __next__(self):
+        """Each time we call next() over a `DevScanner` object, it will return
+           an iterator with the whole currently-available list of devices.
+        """ 
+        res = self.scanner.scan(self.wait_time)
+        return filter(None, (Device(d) for d in res))
+
+
+class Device:
+    """Represents a device.
+
+    Given a bluepy device object, it gets the scan data and looks for switchbot
+    meter information. If found, parses it and populates itself.
+    
+    A device will test falsy if it's not a switchbot meter device, wich is used
+    with a filter(None, devices) to filter out non-switchbot devices from scan
+    data.
+
+
+    You can access the following data properties after initialization:
+
+        - mac: Device mac
+        - model: Device model
+        - mode: Device mode
+        - date: Date of the current scan 
+        - temp: Temperature as reported by the meter
+        - humidity: Humidity, percentage.
+        - data: Complete dict with all the data minus the mac.
+    """
+    def __init__(self, device):
+        self.device = device
+        self.mac = None
+        self.data = {}
+        actions = {
+            '16b Service Data': self.set_service_data,
+            'Local name': self.set_mac,
+            'Complete 128b Services': self.set_mac
+        }
+        for (_, key, value) in self.device.getScanData():
+            # Load data
+            actions.get(key, lambda x: {})(value)
+
+    def __getattr__(self, attr):
+        """Enable direct access to data attributes"""
+        if attr in self.data:
+            return self.data[attr]
+
+    def __bool__(self):
+        """Return false if the device is not a switchbot meter"""
+        return bool(self.mac and self.data)
+
+    def __repr__(self):
+        """Represent data model, temp, humidity and mac."""
+        if self.data:
+            return (f'<{self.data["model"]} temp: {self.data["temp"]} '
+                    f'humidity: {self.data["humidity"]}> ({self.mac})')
+        return 'Unknown device'
+
+    def set_mac(self, value):
+        """Set device mac."""
+        if value in ('WoHand', 'WoMeter', SERVICE_UUID):
+            self.mac = self.device.addr
+
+    def set_service_data(self, value):
+        """Extract service data"""
+        model = binascii.a2b_hex(value[4:6])
+        mode = binascii.a2b_hex(value[6:8])
+
+        if len(value) != 16:
+            return
+
+        temp_fra = int(value[11:12].encode('utf-8'), 16) / 10.0
+        temp_int = int(value[12:14].encode('utf-8'), 16)
+
+        if temp_int < 128:
+            temp_int *= -1
+            temp_fra *= -1
+        else:
+            temp_int -= 128
+
+        humidity = int(value[14:16].encode('utf-8'), 16) % 128
+        self.data = dict(model=model.decode(),
+                         mode=binascii.hexlify(mode).decode(),
+                         date=datetime.datetime.now(),
+                         temp=temp_int + temp_fra,
+                         humidity=humidity)
